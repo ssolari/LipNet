a1078bac06860bd6ef1b4e3dd6ae48515781d88d
ssolari <sorensolari@gmail.com>
2019-01-31 15:41:45 -0800

    modifications for stable conda environment build

--- setup.py
+++ setup.py
@@ -11,16 +11,17 @@ setup(name='lipnet',
     zip_safe=False,
 	install_requires=[
         'Keras==2.0.2',
-        'editdistance==0.3.1',
+        'editdistance==0.5.2',
 		'h5py==2.6.0',
 		'matplotlib==2.0.0',
 		'numpy==1.12.1',
 		'python-dateutil==2.6.0',
 		'scipy==0.19.0',
 		'Pillow==4.1.0',
-		'tensorflow-gpu==1.0.1',
+		# 'tensorflow-gpu==1.0.1',
+		'tensorflow==1.0.0',  # non-gpu version
 		'Theano==0.9.0',
         'nltk==3.2.2',
-        'sk-video==1.1.7',
-        'dlib==19.4.0'
+        'sk-video==1.1.10',
+        # 'dlib==19.4.0'  # let conda handle dlib install
     ])
